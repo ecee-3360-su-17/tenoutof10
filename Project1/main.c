82e92e0883a420a3c034b7c5126abad7f6a11e3f
megvader <margaret.vader@colorado.edu>
2017-06-08 19:06:31 -0600

    main with blink, but blink still needs LED info to run

--- Project1/main.c
+++ Project1/main.c
@@ -5,3 +5,55 @@ int main(void) {
 	
 	return 0;
 }
+
+void blink(int x){
+    int mout = 0b0;
+    while(x>=0){
+       y = x%10
+       int new = convert_to_morose(y);
+       mout = mout | new;
+       mout = mout << 5;
+       x = (x-y)/10;
+    }
+
+    registerbusinessforLED = shift in mout one bit at a time;
+    have a wait
+
+
+}
+
+char convert_to_morose(int n){
+    switch(n){
+    case 0:
+        return 0b11111;
+        break;
+    case 1:
+        return 0b01111;
+        break;
+    case 2:
+        return 0b00111;
+        break;
+    case 3:
+        return 0b00011;
+        break;
+    case 4:
+        return 0b00001;
+        break;
+    case 5:
+        return 0b00000;
+        break;
+    case 6:
+        return 0b10000;
+        break;
+    case 7:
+        return 0b11000;
+        break;
+    case 8:
+        return 0b11100;
+        break;
+    case 9:
+        return 0b11110;
+        break;
+    default:
+        break;
+}
