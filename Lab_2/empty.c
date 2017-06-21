9483b8311cb85f881100e78a1eb506a2b99db127
ldgill <lianne.gill@colorado.edu>
2017-06-21 15:40:00 -0600

    Lab 2 stuff

--- Lab_2/empty.c
+++ Lab_2/empty.c
@@ -53,10 +53,48 @@
 /* Board Header file */
 #include "Board.h"
 
-#define TASKSTACKSIZE   512
+#include <stdint.h>
+
+#define TASKSTACKSIZE     768
 
 Task_Struct task0Struct;
 Char task0Stack[TASKSTACKSIZE];
+
+
+
+/*
+ *  ======== echoFxn ========
+ *  Task for this function is created statically. See the project's .cfg file.
+ */
+Void echoFxn(UArg arg0, UArg arg1)
+{
+    char input;
+    UART_Handle uart;
+    UART_Params uartParams;
+    const char echoPrompt[] = "\fEchoing characters:\r\n";
+
+    /* Create a UART with data processing off. */
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.readEcho = UART_ECHO_OFF;
+    uartParams.baudRate = 9600;
+    uart = UART_open(Board_UART0, &uartParams);
+
+    if (uart == NULL) {
+        System_abort("Error opening the UART");
+    }
+
+   UART_write(uart, echoPrompt, sizeof(echoPrompt));
+
+    /* Loop forever echoing */
+    while (1) {
+        UART_read(uart, &input, 1);
+        UART_write(uart, &input, 1);
+    }
+}
+
 
 /*
  *  ======== heartBeatFxn ========
@@ -70,7 +108,6 @@ Void heartBeatFxn(UArg arg0, UArg arg1)
         GPIO_toggle(Board_LED0);
     }
 }
-
 /*
  *  ======== main ========
  */
@@ -89,6 +126,13 @@ int main(void)
     // Board_initWatchdog();
     // Board_initWiFi();
 
+    Task_Params taskParams1;
+    Task_Params_init(&taskParams1);
+    taskParams1.stackSize = TASKSTACKSIZE;
+    taskParams1.stack = &task0Stack;
+    taskParams1.instance->name = "echo";
+    Task_construct(&task0Struct, (Task_FuncPtr)echoFxn, &taskParams1, NULL);
+
     /* Construct heartBeat Task  thread */
     Task_Params_init(&taskParams);
     taskParams.arg0 = 1000;
@@ -96,11 +140,16 @@ int main(void)
     taskParams.stack = &task0Stack;
     Task_construct(&task0Struct, (Task_FuncPtr)heartBeatFxn, &taskParams, NULL);
 
+
+
     /* Turn on user LED */
     GPIO_write(Board_LED0, Board_LED_ON);
 
-    System_printf("Starting the example\nSystem provider is set to SysMin. "
-                  "Halt the target to view any SysMin contents in ROV.\n");
+
+   System_printf("Starting the UART Echo example\nSystem provider is set to "
+                     "SysMin. Halt the target to view any SysMin contents in "
+                     "ROV.\n");
+
     /* SysMin will only print to the console when you call flush or exit */
     System_flush();
 
